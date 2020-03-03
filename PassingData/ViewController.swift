fec22f2da3883f3f50bce00609b5d0d8026f2b99
AdithyaSai Neeli <neeliadithyasai@gmail.com>
2020-03-03 18:27:08 -0500

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by MacStudent on 2020-03-03.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
